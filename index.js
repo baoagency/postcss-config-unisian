ec068facf9b1011beca808afb93ea80691af9d37
Dan Gamble <dan@dangamble.co.uk>
2019-01-16 16:47:54 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+module.exports = ({ file, options, env }) => ({
+  plugins: {
+    'postcss-easy-import': {
+      extensions: '.pcss'
+    },
+    'postcss-preset-env': {
+      stage: 2,
+      features: {
+        'custom-media-queries': true,
+        'custom-properties': {
+          preserve: false
+        },
+        'custom-selectors': true
+      }
+    },
+    'postcss-sassy-mixins': {},
+    'postcss-nested': {},
+    // Niceties
+    'postcss-inline-svg': {
+      path: 'src/svgs/'
+    },
+    'postcss-brand-colors': {},
+    'postcss-property-lookup': {},
+    'postcss-pxtorem': {},
+    'postcss-will-change': {},
+    'postcss-round-subpixels': {},
+    'postcss-hexrgba': {},
+    cssnano: env === 'production'
+  }
+})
