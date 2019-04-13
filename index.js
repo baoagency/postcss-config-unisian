bb8564778a55a7a41a5e4471425b251b8b954846
Dan Gamble <dan@dangamble.co.uk>
2019-04-13 13:05:40 +0100

    Allow custom config to override the whole default config

--- index.js
+++ index.js
@@ -1,33 +1,32 @@
-module.exports = ({ customPropertiesPath }) => {
-  return ({ file, options, env }) => ({
-    plugins: {
-      'postcss-easy-import': {
-        extensions: '.pcss'
-      },
-      'postcss-preset-env': {
-        stage: 2,
-        features: {
-          'custom-media-queries': true,
-          'custom-properties': {
-            preserve: false,
-            exportTo: customPropertiesPath
-          },
-          'custom-selectors': true
-        }
-      },
-      'postcss-sassy-mixins': {},
-      'postcss-nested': {},
-      // Niceties
-      'postcss-inline-svg': {
-        path: 'src/svgs/'
-      },
-      'postcss-brand-colors': {},
-      'postcss-property-lookup': {},
-      'postcss-pxtorem': {},
-      'postcss-will-change': {},
-      'postcss-round-subpixels': {},
-      'postcss-hexrgba': {},
-      cssnano: env === 'production'
+const defaultPlugins = ({ env }) => ({
+  'postcss-easy-import': {
+    extensions: '.pcss'
+  },
+  'postcss-preset-env': {
+    stage: 2,
+    features: {
+      'custom-media-queries': true,
+      'custom-properties': true,
+      'custom-selectors': true
     }
+  },
+  'postcss-sassy-mixins': {},
+  'postcss-nested': {},
+  // Niceties
+  'postcss-inline-svg': {
+    path: 'src/svgs/'
+  },
+  'postcss-brand-colors': {},
+  'postcss-property-lookup': {},
+  'postcss-pxtorem': {},
+  'postcss-will-change': {},
+  'postcss-round-subpixels': {},
+  'postcss-hexrgba': {},
+  cssnano: env === 'production'
+})
+
+module.exports = ({ customConfig }) => {
+  return ({ file, options, env }) => ({
+    plugins: Object.assign({}, defaultPlugins({ env }), customConfig)
   })
 }
