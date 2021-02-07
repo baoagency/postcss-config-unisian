44ab1edafa9e2232c36bab13deae0baa4f5694ab
Dan Gamble <dan@dangamble.co.uk>
2021-02-07 15:48:21 +0000

    Remove mergeLonghand from cssnano because it changes CSS variable names for some reason

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const defaultPlugins = ({ env }) => ({
   'postcss-easy-import': {
-    extensions: '.pcss'
+    extensions: '.pcss',
   },
   'postcss-preset-env': {
     stage: 2,
     features: {
       'custom-media-queries': true,
       'custom-properties': true,
-      'custom-selectors': true
-    }
+      'custom-selectors': true,
+    },
   },
   'postcss-sassy-mixins': {},
   'postcss-nested': {},
   // Niceties
   'postcss-inline-svg': {
-    path: 'src/svgs/'
+    path: 'src/svgs/',
   },
   'postcss-brand-colors': {},
   'postcss-property-lookup': {},
@@ -23,10 +23,19 @@ const defaultPlugins = ({ env }) => ({
   'postcss-round-subpixels': {},
   'postcss-hexrgba': {},
   cssnano: env === 'production'
+    ? {
+      preset: [
+        'default',
+        {
+          mergeLonghand: false,
+        },
+      ],
+    }
+    : false,
 })
 
 module.exports = ({ customConfig }) => {
   return ({ file, options, env }) => ({
-    plugins: Object.assign({}, defaultPlugins({ env }), customConfig({ env }))
+    plugins: Object.assign({}, defaultPlugins({ env }), customConfig({ env })),
   })
 }
