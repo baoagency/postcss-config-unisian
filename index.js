83bf4a55a377cf07368ce119f8cf210b7265a5c3
Dan Gamble <dan@dangamble.co.uk>
2022-09-10 18:14:38 +0100

    2.0.0

--- index.js
+++ index.js
@@ -6,22 +6,12 @@ const defaultPlugins = ({ env }) => ({
     stage: 2,
     features: {
       'custom-media-queries': true,
-      'custom-properties': true,
-      'custom-selectors': true,
     },
   },
-  'postcss-sassy-mixins': {},
   'postcss-nested': {},
   // Niceties
-  'postcss-inline-svg': {
-    path: 'src/svgs/',
-  },
   'postcss-brand-colors': {},
   'postcss-property-lookup': {},
-  'postcss-pxtorem': {},
-  'postcss-will-change': {},
-  'postcss-round-subpixels': {},
-  'postcss-hexrgba': {},
   cssnano: env === 'production'
     ? {
       preset: [
